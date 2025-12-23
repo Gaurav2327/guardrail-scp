7c91d18e13fc87c34429441d7f7845058c91cc2d
Gaurav Dhapola <79783906+Gaurav2327@users.noreply.github.com>
2025-12-24 00:59:18 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_version = "~> 1.11"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.0"
+    }
+  }
+
+  backend "s3" {
+    key = "terraform/backend/scp.tfstate"
+    bucket = "terraform-state-bucket-dops"
+    encrypt = true
+  }
+}
